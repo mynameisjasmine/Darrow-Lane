e939906cbca14008d563c4d91cbc648176eeaef5
Jasmine <jasminecharles@hotmail.com>
2021-08-22 20:54:43 -0700

    top row pics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import './index.css'
 import Navigation from './components/Navigation';
+import DarrowLane from './components/DarrowLane';
 
 
 
@@ -15,6 +16,7 @@ function App() {
       <div className='border'>Free Shipping with $50 purchase or free Store Pickup</div>
       <header>
         <Navigation />
+        {/* <DarrowLane /> */}
       </header>
     </div>
   );
--- src/components/DarrowLane.js
+++ src/components/DarrowLane.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const DarrowLane = () => {
     return (
-
-    <h1>DarrowLane Component</h1>
-    
+    <div className='main-image-div'>
+    {/* <h1>DarrowLane Component</h1> */}
+        <div className='image-div'><img src={'./assets/mod1.png'}/></div>
+        <div className='image-div'><img src={'./assets/mod2.png'}/></div>
+        <div className='image-div'><img src={'./assets/mod3.png'}/></div>
+        <div className='image-div'><img src={'./assets/mod4.png'}/></div>
+    </div>
     )
 }
 
